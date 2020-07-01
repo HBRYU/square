18a695a0af9b0a2146d87a8632ec4c4a4ef0c03f
lkh3409 <lkh3409@naver.com>
2020-07-01 16:00:00 +0900

    square move
    10421 이강훈

---
+++ New Unity Project/Assets/move.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 1;
+    void Update()
+    {
+        Vector2 position = transform.position;
+        if (Input.GetKey(KeyCode.A))
+        {
+            position.x += -0.1f * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            position.x += 0.1f * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            position.y += -0.1f * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            position.y += 0.1f * speed * Time.deltaTime;
+        }
+        transform.position = position;
+    }
+}
---
+++ New Unity Project/Assets/spin.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class spin : MonoBehaviour
+{
+
+   private void Update()
+    {
+        UnityEngine.Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        UnityEngine.Quaternion rotation = UnityEngine.Quaternion.AngleAxis(angle - 90, UnityEngine.Vector3.forward);
+        transform.rotation = rotation;
+
+
+
+    }
+}
